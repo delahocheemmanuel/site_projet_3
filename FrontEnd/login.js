5b2be22b02344b9e4adca3598fbea0e43ed0ef93
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-03-10 02:02:14 +0100

    modif boutton filtre avec position active au click avec ajout .active dans le css, les bouttons sont blanc à l actu . vert au survol et vert au clique.

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,44 +1,37 @@
-const loginURL = "http://localhost:5678/api/users/login";
+// Récupération des informations d'authentification à partir du FORM de LOGIN.HTML
+const formInfos = document.querySelector("form");
 
-function getUserLog() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    const user = {
-        email: email,
-        password: password
-    }
-    return user;
-}
-function showErrorMsg() {
-    alert("Erreur dans l'identifiant ou le mot de passe");
-}
+// Fonction d'authentification à partir des informations de l'API
 
-document.getElementById('btn__login').addEventListener('click', function(e) {
-    e.preventDefault();
-    let user = getUserLog();
-    fetch(loginURL, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then((res) => {
-        if (res.ok) {
-            return res.json();
-        }
-        else {
-            showErrorMsg();
-            return 0;
-        }
-    })
-    .then((value) => {
-        if (value !== 0) {
-            localStorage.setItem("token", value.token);
-            location.href = "index.html";
-        }
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
-}); 
+formInfos.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    // Création de l'objet "userInfos"
+	const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+	const userInfos = {email,password};
+
+	// Envoi de ces informations sur l'API "LOGIN"
+	const authentificationInfos = await fetch("http://localhost:5678/api/users/login", {
+	method: "POST",
+	headers: {"Content-Type": "application/json"},
+	body: JSON.stringify(userInfos)
+  	});
+
+	// Récupération de la réponse et traduction
+	const authentificationResponse = await authentificationInfos.json();
+	// Stockage du TOKEN du User identifié et de l'état de l'authentification ---------
+	const authentificationToken = authentificationResponse.token;
+	const authentificationState = authentificationInfos.ok;
+	// --------------------------------------------------------------------------------
+
+	// Retour conditionnel en fonction de l'authentification et stockage des données dans le "sessionStorage".
+	if (authentificationState === true) {
+		sessionStorage.setItem("authentificationToken", authentificationToken);
+		sessionStorage.setItem("authentificationState", authentificationState);
+		window.location.replace("index.html");
+	} else {
+		sessionStorage.setItem("authentificationState", authentificationState);
+		const wrongUserNotification = document.querySelector(".wrong-user-notification");
+		wrongUserNotification.innerText = "Nom d'utilisateur ou mot de passe incorrect.";
+	};
+});
